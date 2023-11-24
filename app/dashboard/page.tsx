d898fe74105f4acab23fa2001a2d1afa08586d33
Andrés Toala <ai_toala@outlook.com>
2023-11-23 19:31:08 -0500

    Refactor dashboard page and update latest invoices and revenue chart components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,30 @@
-export default function DashboardPage() {
+import { Suspense } from 'react';
+import { Card } from '../ui/dashboard/cards';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+import RevenueChart from '../ui/dashboard/revenue-chart';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '../ui/skeletons';
+export default async function DashboardPage() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Dashboard content</p>
-    </div>
+    <main>
+      <h1 className="mb-4 text-xl md:text-2xl">Dashboard</h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Collected"
+          value={numberOfCustomers}
+          type="customers"
+        /> */}
+      </div>
+      <div>
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
   );
 }
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,4 @@
-import { Urbanist } from 'next/font/google';
-
+import { Lusitana, Urbanist } from 'next/font/google';
 export const urban = Urbanist({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
